14b8338c5a15b0a23adc965ced8ae95defd2f8f8
krezon123 <krezon@yandex.ru>
2024-08-26 14:43:52 +0300

    Homewor9

---
+++ main.js
@@ -0,0 +1,47 @@
+// Домашняя работа на 9 урок
+
+// let btnmas = document.querySelector('.btnmas')
+// let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
+// btnmas.onclick = function(){
+//     a.splice(5,6)
+//     console.log(a);
+
+// }
+
+
+
+// let btnmas = document.querySelector('.btnmas')
+// btnmas.onclick = function(){
+    
+//     const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+//     const even = arr.filter(number => {
+//     return number % 2 ===0;
+// });
+//     console.log(even);
+// }
+
+
+
+// let btnmas = document.querySelector('.btnmas')
+// btnmas.onclick = function(){
+    
+//     const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+//     const odds = arr.filter(number => {
+//     return number % 2 !== 0;
+// });
+//     console.log(odds);
+// }
+
+
+let a = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [10, 11]
+];
+a.reverse();
+console.log(a);
+
+
+
